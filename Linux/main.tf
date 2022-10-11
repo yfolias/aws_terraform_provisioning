e6547abd9faecd4bd7772cf7680191beca0d673d
yfolias <yannis.folias@gmail.com>
2022-10-11 20:50:16 +0100

    Commiting files for linux ec2 instance provisioning

---
+++ Linux/main.tf
@@ -0,0 +1,52 @@
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.16.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+}
+
+resource "aws_internet_gateway" "my_gw" {
+    vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_subnet" "my_subnet" {
+    vpc_id            = aws_vpc.my_vpc.id
+    cidr_block        = "10.16.10.0/24"
+    availability_zone = var.availability_zone
+}
+
+resource "aws_route_table" "route_table" {
+    vpc_id = aws_vpc.my_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my_gw.id
+    }
+}
+
+resource "aws_route_table_association" "subnet-association" {
+    subnet_id      = aws_subnet.my_subnet.id
+    route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_key_pair" "my_key" {
+    key_name   = var.ssh_key_name
+    public_key = var.public_key
+}
+
+resource "aws_instance" "instance" {
+    ami           = var.ami_id
+    instance_type = var.instance_type
+    key_name      = aws_key_pair.my_key.key_name
+    associate_public_ip_address   =   true
+    security_groups = [aws_security_group.ingress-all-test.id]
+    subnet_id = aws_subnet.my_subnet.id
+    tags = {
+        Name = var.instance_name
+    }
+}
+
+output "instance_ip" {
+    description = "The public ip for ssh access"
+    value       = aws_instance.instance.public_ip
+}
---
+++ Linux/variables.tf
@@ -0,0 +1,39 @@
+variable "region" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "token" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "public_key" {
+  type = string
+}
+
+variable "ssh_key_name" {
+  
+}
